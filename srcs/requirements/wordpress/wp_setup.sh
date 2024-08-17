c036b605866eb16b2200b83257c73e2b0f3b2522
Dani X <danixidev@gmail.com>
2024-08-17 16:19:39 +0200

    Change from latests tu buster

--- srcs/requirements/wordpress/wp_setup.sh
+++ srcs/requirements/wordpress/wp_setup.sh
@@ -3,7 +3,7 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname=$DB_DATABASE --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASSWORD --dbhost=$DB_HOSTNAME --allow-root
+./wp-cli.phar core install --url=$DOMAIN_NAME --title=Inception --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --allow-root
 
-php-fpm8.2 -F
+php-fpm7.3 -F
